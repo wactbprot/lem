fdb8abdeaab65180f5b3db4a0d21f2d71a866d78
wactbprot <thsteinbock@web.de>
2015-05-17 13:49:35 +0200

    ok, next time ets

--- src/store.erl
+++ src/store.erl
@@ -10,9 +10,19 @@
 -export([generate/2]).
 
 generate(Path, Value) ->
-    ?DEBUG(Path),
+    [H|T] = Path,
+    TableId = list_to_atom(H),
+    Atm     = gen_lvl_atm(length(T) - 1),
+    Chain   = gen_chain(Atm, T),
+    ?DEBUG(T),
+    ?DEBUG(Atm),
+    ?DEBUG(Chain),
+    ?DEBUG(TableId),
     ?DEBUG(Value).
 
-gen_path(Path) ->
-    N = length(Path) -1,
-    
+
+gen_lvl_atm(N) ->
+    [list_to_atom("level_" ++ [I]) || I <- lists:seq(97, 97 + N)]. 
+
+gen_chain(L,T) -> 
+    lists:zip(L,T).
